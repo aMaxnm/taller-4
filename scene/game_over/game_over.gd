6d1dcbc319884126f53314bb10dfda72ea0c1dd2
Ramiro Pimentel <ramiro.pimentel@uabc.edu.mx>
2026-01-08 16:25:40 -0800

    gameover

---
+++ scene/game_over/game_over.gd
@@ -0,0 +1,29 @@
+extends CanvasLayer
+
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var continue_label: Label = $MarginContainer/ContinueLabel
+
+var can_return_to_menu: bool = false
+
+func _ready() -> void:
+	visible = false
+	can_return_to_menu = false
+
+	SignalManager.on_game_over.connect(show_game_over)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("space") and can_return_to_menu:
+		get_tree().paused = false
+		SignalManager.on_restart_game.emit()
+
+func show_game_over() -> void:
+	print("gameover")
+	get_tree().paused = true
+	visible = true
+
+	continue_label.visible = false
+	anim_player.play("GO")
+	
+	continue_label.visible = true
+	anim_player.queue("continue_label")
+	can_return_to_menu = true
--- singletons/GameManager.gd
+++ singletons/GameManager.gd
@@ -1,14 +1,24 @@
 extends Node
+<<<<<<< Updated upstream
 
 var menu_scene: PackedScene = preload("res://scene/menu/menu.tscn")
 
 var lives: int = 3
 var score: int = 0
 var is_paused: bool = false 
+=======
+ 
+var menu_scene: PackedScene = preload("res://scene/test_level/test_level.tscn")
+var lives: int = 3
+var score: int = 0
+@onready var player = $Player
+
+>>>>>>> Stashed changes
 
 func _ready() -> void:
 	SignalManager.on_hurt.connect(on_hurt_received)
 	SignalManager.on_bonus_grabbed.connect(on_bonus_grabbed)
+<<<<<<< Updated upstream
 	SignalManager.toggle_pause.connect(_on_toggle_pause)
 	SignalManager.on_restart_game.connect(returnToMenu)
 
@@ -19,11 +29,25 @@ func returnToMenu():
 func _on_toggle_pause() -> void:
 	is_paused = not is_paused
 	get_tree().paused = is_paused
+=======
+	SignalManager.on_restart_game.connect(return_to_menu)
+>>>>>>> Stashed changes
 
 func on_hurt_received():
 	lives -= 1
 	SignalManager.on_lives_changed.emit(lives)
 
+	# Si se acaban las vidas
+	if lives <= 0:
+		SignalManager.on_game_over.emit()
+
 func on_bonus_grabbed(bonus_score: int):
 	score += bonus_score
 	SignalManager.on_score_changed.emit(score)
+
+func return_to_menu():
+	get_tree().paused = false
+	score = 0
+	lives = 3  # Reinicia las vidas
+	SignalManager.on_lives_changed.emit(lives) # Notifica a la UI
+	get_tree().change_scene_to_packed(menu_scene)
